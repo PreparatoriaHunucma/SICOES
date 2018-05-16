bf9a6e961568304c1f9a70739eabe5cc9747606a
Ricardo Chan <josericardochan@hotmail.com>
2018-05-16 11:33:55 -0500

    DocumentosAlumnosDAO

--- SICOES2018/SICOES2018/DAO/DocumentosAlumnoDAO.cs
+++ SICOES2018/SICOES2018/DAO/DocumentosAlumnoDAO.cs
@@ -14,10 +14,19 @@ namespace SICOES2018.DAO
         //Para modificar el estado del aviso segun el alumno [0 = NO VISTO, 1 = VISTO]
         public int modificarDocumentos(DocumentosAlumnoBO datosDocumentosAlumno)
         {
-            cmd = new SqlCommand("UPDATE DocumentosAlumno SET ActaNacimiento, Fotografias, Curp, Constancia, ComprobanteDomiciliario, BoletaCalificaciones, , CertificadoParcial, OficioRevalidacion, ConstanciaMedia, CertificadoSecundaria, Otros = @ActaNacimiento WHERE IDDocumentos = @IDDocumentos");
+            cmd = new SqlCommand("UPDATE DocumentosAlumno SET ActaNacimiento = @ActaNacimiento, Fotografias = @Fotografias, Curp = @Curp, Constancia = @Constancia, ComprobanteDomiciliario = @ComprobanteDomiciliario, BoletaCalificaciones = @BoletaCalificaciones, CertificadoParcial = @CertificadoParcial, OficioRevalidacion = @OficioRevalidacion, ConstanciaMedia = @ConstanciaMedia, CertificadoSecundaria = @CertificadoSecundaria, Otros = @Otros WHERE IDDocumentos = @IDDocumentos");
 
             cmd.Parameters.Add("@ActaNacimiento", SqlDbType.TinyInt).Value = datosDocumentosAlumno.ActaNacimiento;
-
+            cmd.Parameters.Add("@Fotografias", SqlDbType.TinyInt).Value = datosDocumentosAlumno.Fotografias;
+            cmd.Parameters.Add("@Curp", SqlDbType.TinyInt).Value = datosDocumentosAlumno.Curp;
+            cmd.Parameters.Add("@Constancia", SqlDbType.TinyInt).Value = datosDocumentosAlumno.Constancia;
+            cmd.Parameters.Add("@ComprobanteDomiciliario", SqlDbType.TinyInt).Value = datosDocumentosAlumno.CompromanteDomiciliario;
+            cmd.Parameters.Add("@BoletaCalificaciones", SqlDbType.TinyInt).Value = datosDocumentosAlumno.BoletaCalificaciones;
+            cmd.Parameters.Add("@CertificadoParcial", SqlDbType.TinyInt).Value = datosDocumentosAlumno.CertificadoParcial;
+            cmd.Parameters.Add("@OficioRevalidacion", SqlDbType.TinyInt).Value = datosDocumentosAlumno.OficioRevalidacion;
+            cmd.Parameters.Add("@ConstanciaMedia", SqlDbType.TinyInt).Value = datosDocumentosAlumno.ConstanciaMedia;
+            cmd.Parameters.Add("@CertificadoSecundaria", SqlDbType.TinyInt).Value = datosDocumentosAlumno.CertificadoSecundaria;
+            cmd.Parameters.Add("@Otros", SqlDbType.TinyInt).Value = datosDocumentosAlumno.Otros1;
             cmd.Parameters.Add("@IDDocumentos", SqlDbType.Int).Value = datosDocumentosAlumno.IDDocumentos;
             cmd.CommandType = CommandType.Text;
 
